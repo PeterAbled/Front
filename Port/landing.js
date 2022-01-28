a4997c16cf8bf61e67306c1f2c818a7a55c8b7ba
PeterAbled <98515903+PeterAbled@users.noreply.github.com>
2022-01-28 13:46:18 +0900

    Add files via upload

--- Port/landing.js
+++ Port/landing.js
@@ -5,14 +5,7 @@ const SLIDE_WIDTH = 1200; // 슬라이드 시 넘어갈 화면 크기
 const prevBtn = document.querySelector(".prev")
 const nextBtn = document.querySelector(".next")
 const slides = document.querySelector(".slideShow")
-const slidesIndex = document.querySelector(".slideShow li")
 const likeBtn = document.querySelector(".like_Btn")
-let about = document.querySelector(".screen1_1")
-
-
-function aboutOpen() {
-  about.style.transform = `scale(2)`;
-}
 
 function prev() {
   if(curPos > 0) {
@@ -41,34 +34,34 @@ function like_Btn() {
   const name = document.querySelector("#name");
   if(like == 0) {
     likeBtn.innerHTML = '<i class="fa fa-heart" aria-hidden="true"></i> You liked this';
-    name.innerText = 'Happy';
+    // name.innerText = 'Happy';
     const proPic = document.querySelector("#info_Name");
     proPic.removeAttribute("src");
     proPic.setAttribute("src", "image/liked.png");
     like = 1;
   } else if(like == 1) {
     likeBtn.innerHTML = '<i class="fa fa-heart-broken" aria-hidden="true"></i> You hated this';
-    name.innerText = 'Sad'
+    // name.innerText = 'Sad'
     const proPic = document.querySelector("#info_Name");
     proPic.removeAttribute("src");
     proPic.setAttribute("src", "image/hated.png");
     like = -1;
   } else if(like == -1){
     likeBtn.innerHTML = '<i class="fa fa-heart" aria-hidden="true"></i> You liked this';
-    name.innerText = 'Happy'
+    // name.innerText = 'Happy'
     const proPic = document.querySelector("#info_Name");
     proPic.removeAttribute("src");
     proPic.setAttribute("src", "image/liked.png");
     like = 1;
   }
-} 
+}
 
 function init() {
   prevBtn.setAttribute('disabled', 'true')
   prevBtn.addEventListener("click", prev)
   nextBtn.addEventListener("click", next)
   likeBtn.addEventListener("click", like_Btn)
-  about.addEventListener("click", aboutOpen)
 }
  
 init();
+
